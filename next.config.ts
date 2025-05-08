7eeeb9225dee604a4093be6e09e3051731743d48
Joss Bleach <joss.bleach@outlook.com>
2025-05-08 21:21:03 +0100

    init + shadcn setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import { env } from "@/config/env";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: env.VERCEL_ENV !== "production" ? true : false,
+  },
+  experimental: {
+    reactCompiler: true,
+  },
 };
 
 export default nextConfig;
