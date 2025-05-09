e69fd76a670cd560debe465aa7b27a32933cc00c
Joss Bleach <joss.bleach@outlook.com>
2025-05-09 16:31:46 +0100

    updated schemas

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,1 +1,7 @@
 export * from "./exercises";
+export * from "./template_exercises";
+export * from "./workout_templates";
+export * from "./workouts";
+export * from "./sets";
+export * from "./exercise_bests";
+export * from "./weekly_volume";
