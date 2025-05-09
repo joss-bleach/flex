637c435c8ec15e262c81b80deaf039ee8028ff4b
Joss Bleach <joss.bleach@outlook.com>
2025-05-09 17:33:16 +0100

    set up protected procedure

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,14 +1,31 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { cache } from "react";
 import superjson from "superjson";
+import { auth } from "@clerk/nextjs/server";
+
 export const createTRPCContext = cache(async () => {
-  return { userId: "user_123" };
+  const { userId } = await auth();
+  return { userId };
 });
 
-const t = initTRPC.create({
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
+
+const t = initTRPC.context<Context>().create({
   transformer: superjson,
 });
 
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const protectedProcedure = t.procedure.use(async function auth(opts) {
+  const { ctx, next } = opts;
+  if (!ctx.userId) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return opts.next({
+    ctx: {
+      ...ctx,
+    },
+  });
+});
